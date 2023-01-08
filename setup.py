7fef384a58a8635dd827e453f6f0afc794289ef9
mansoor hassan <mansoor_jis@yahoo.co.in>
2023-01-08 17:23:00 +0000

    tox setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import  setup,find_packages
+
+setup (
+    name = "prod",
+    packages= find_packages()
+
+
+)
---
+++ tests/test_prod.py
@@ -0,0 +1,3 @@
+from prod import calc
+def test_calc():
+    assert calc(3,2) == 6
